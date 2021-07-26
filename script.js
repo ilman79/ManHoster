0a92e19f413ba04fbb65378aca9f32cf8b91b0bd
ilmangifari <gifariilman79@gmail.com>
2021-07-26 15:02:42 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('nav ul');
+
+menu.addEventListener('click', function () {
+   nav.classList.toggle('slide');
+});
+
+nav.addEventListener('click', function () {
+   nav.classList.toggle('slide');
+});
+
+
+
+
